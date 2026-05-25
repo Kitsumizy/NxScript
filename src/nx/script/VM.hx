be1e5cd44eaa63ae6fbe104660e5846b0ac0e342
Freya <transgendergamming@gmail.com>
2026-05-25 16:45:12 -0400

    Patch VDict And Anon Functions Null Return (#31)
    * Patch VDict Null Return
    
    this fixes vdict returning null on host.
    
    * Patch Anonymous Functions on Host
    
    Fixes Anonymous functions from returning null on Host.

--- src/nx/script/VM.hx
+++ src/nx/script/VM.hx
@@ -1913,6 +1913,17 @@ class VM {
 					case VNativeObject(_): NxProxy.wrapInstanceValue(value, this); // bind callbacks on native-backed script instances
 					default: null;
 				}
+			case VDict(map):
+				var obj:Dynamic = {};
+				for (k in map.keys())
+					Reflection.setField(obj, k, valueToHaxe(map.get(k)));
+					obj;
+			case VFunction(funcChunk, closure):
+				// Haxe will call this with whatever args the native passes
+				Reflect.makeVarArgs((args:Array<Dynamic>) -> {
+					var scriptArgs = [for (a in args) haxeToValue(a)];
+					return valueToHaxe(callFunction(funcChunk, closure, scriptArgs));
+				});
 			default: null;
 		}
 	}
